bcb496d358b68deefe90c6b00928432dd8947dac
LRomanTomas <tomasroman07@hotmail.com>
2025-09-15 19:31:16 -0300

    Sprint 1 Home|Login|Quien-soy

--- src/app/services/github.ts
+++ src/app/services/github.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +11,7 @@ export class GithubService {
   constructor() {}
 
   getUser(username: string) {
-    const headers = new HttpHeaders().set('Authorization', 'Bearer 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').set('X-GitHub-Api-Version', '2022-11-28');
+    const headers = new HttpHeaders().set('Authorization', 'Bearer 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').set('X-GitHub-Api-Version', '2022-11-28');
     const observable = this.http_client.get<any>(`https://api.github.com/users/${username}`, {headers});
     return observable;
   }
